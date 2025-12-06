0199c6fb0c55be742382790794ea05a2f7e50161
HeakMeng <phengmengheak@outlook.com>
2025-12-07 00:40:27 +0700

    created index.js for start the server

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import app from "./app.js";
+import  dotenv from "dotenv";
+import connectDB from "./config/database.js";
+
+dotenv.config({
+  path: './.env',
+})
+
+const startServer = async () => {
+  try{
+    await connectDB();
+    app.on("error", (error) => {
+      console.log("Error",error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 4569, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  } catch(error){
+    console.log("MongoDB connection failed!",error);
+  }
+};
+
+startServer();
