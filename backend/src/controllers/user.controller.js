e498b9cfd40ce6625874d4483a7edaa6f55ed47b
HeakMeng <phengmengheak@outlook.com>
2025-12-07 14:23:13 +0700

    Created routes and controller for register user success

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,28 +1,37 @@
-import { User } from "../models/users.model.js";
+import{ User } from "../models/users.model.js";
+import bcrypt from "bcryptjs";
 
 const registerUser = async (req, res) => {
   try{
+    console.log("1. Request received!");
     const { username, email, password} = req.body;
 
     if(!username || !email || !password){
       return res.status(400).json({message: "All fields are important!"});
     }
-    
+    console.log("2. Checking DB...");
     const existing = await User.findOne({email: email.toLowerCase()});
+    console.log("3. User check done. Result:", existing);
     if(existing){
-      return res.status(400).json({message: "User already register! \n Please fill new one!@"}); 
+      return res.status(409).json({message: "User already register! \n Please fill new one!@"}); 
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const user = await User.create({
       username, 
-      password, 
+      password: hashedPassword, 
       email: email.toLowerCase(),
       loggedIn: false
     });
 
     res.status(201).json({
       message: "User registered sucessfully!",
-      user: {id: user._id, email:user.email, username: user.username }
+      user: {
+        id: user._id, 
+        email:user.email, 
+        username: user.username }
     })
   } catch(error){
     res.status(500).json({message: "Internal server error@",error: error.message});
--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -33,4 +33,4 @@ const userSchema = new Schema(
   }
 );
 
-export default User = mongoose.model("User",userSchema);
+export const User = mongoose.model("User", userSchema);
