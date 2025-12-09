1c4074a776dfccae10bf83aa3455a4175d95e2c0
HeakMeng <phengmengheak@outlook.com>
2025-12-09 22:41:12 +0700

    Created api for update user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import{ User } from "../models/users.model.js";
-import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
 const registerUser = async (req, res) => {
   try{
@@ -93,14 +93,46 @@ const getAllUsers = async (req, res) => {
 
 const updateUser = async (req, res) => {
   try{
-    
-  } catch(error){
+    const { id } = req.params;
+    const user = await User.findById(id);
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid User ID format" });
+    }
+
+    if(!user){
+      return res.status(404).json({
+        message: "User not found!!"
+      })
+    };
+
+    if (req.body.username) user.username = req.body.username;
+    if (req.body.email) user.email = req.body.email;
+    
+    if(req.body.password){
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    const userResponse = updatedUser.toObject();
+    delete userResponse.password;
+
+    res.status(200).json({
+      message: "User updated successfully!",
+      user: userResponse
+    });
+
+  } catch(error){
+    console.log(error);
+    res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
+
+
 
 export {
   registerUser,
   getAllUsers,
-  loginUser
+  loginUser,
+  updateUser
 };
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { registerUser ,getAllUsers,loginUser } from "../controllers/user.controller.js";
+import { 
+  registerUser ,
+  getAllUsers,
+  loginUser,
+  updateUser
+ } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.route('/').get(getAllUsers);
 router.route('/register').post(registerUser);
+router.route('/:id').patch(updateUser);
+
 router.route('/login').post(loginUser);
+
 
 export default router;
